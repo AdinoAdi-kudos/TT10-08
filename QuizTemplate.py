c5e7af42d76d1f86e7b3eb0a6da847338ebdfbdc
Adi <162955723+AdinoAdi-kudos@users.noreply.github.com>
2024-05-15 22:44:00 +0800

    Html for Quiz Template, change the quiz format

--- QuizTemplate.py
+++ QuizTemplate.py
@@ -2,19 +2,31 @@ from flask import Blueprint, render_template, request, redirect, url_for
 
 quiz_blueprint = Blueprint('quiz', __name__)
 
-@quiz_blueprint.route('/quiz', methods=['GET', 'POST'])
-@login_required
-def quiz():
-    if request.method == 'POST':
-        # Process quiz answers and calculate score
-        score = 0
-        for question in quiz_questions:
-            answer = request.form[question['id']]
-            if answer == question['correct_answer']:
-                score += 1
-        return render_template('quiz_result.html', score=score)
-    return render_template('quiz.html', questions=quiz_questions)
+# Use a dictionary to store the scores, instead of separate variables
+scores = {'FOE': 0, 'FCI': 0, 'FOM': 0, 'FAC': 0, 'FCM': 0}
 
-quiz_questions = [
-    {'id': 'q1', 'question': 'What is the capital of France?', 'correct_answer': 'Paris'},
-]
+@quiz_blueprint.route('/q1', methods=['GET', 'POST'])
+def q1():
+    if request.method == 'POST':
+        q1s = request.form['q1s']
+        if q1s == "Yes":
+            global scores
+            scores['FOE'] += 1
+            scores['FCI'] += 1
+            scores['FOM'] += 0
+            scores['FAC'] -= 1
+            scores['FCM'] -= 1
+        elif q1s == "No":
+            global scores
+            scores['FOE'] -= 1
+            scores['FCI'] -= 1
+            scores['FOM'] += 0
+            scores['FAC'] += 1
+            scores['FCM'] += 1
+        return redirect(url_for('foe.q2'))
+    return render_template('q1.html')
+
+@quiz_blueprint.route('/q2')
+def q2():
+    # implement q2 logic here
+    pass
